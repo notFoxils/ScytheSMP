751ebf9634b873955a97d619f3fbd1b4776d3249
notFoxils <voltrixz9514@gmail.com>
2024-11-26 20:03:17 -0500

    Quick fix, cursed for loops

--- src/main/java/me/foxils/scythesmp/items/RerollItem.java
+++ src/main/java/me/foxils/scythesmp/items/RerollItem.java
@@ -26,6 +26,8 @@ public class RerollItem extends Item implements InventoryClickAction {
     private void changeToRandomItem(InventoryClickEvent inventoryClickEvent) {
         if (!inventoryClickEvent.getSlotType().equals(InventoryType.SlotType.RESULT)) return;
 
+        final ItemStack clickedItemStack = inventoryClickEvent.getCurrentItem();
+
         final Player player = (Player) inventoryClickEvent.getWhoClicked();
         final PlayerInventory playerInventory = player.getInventory();
 
@@ -40,7 +42,7 @@ public class RerollItem extends Item implements InventoryClickAction {
         final Map<String, Integer> gemLevelMap = playerStats.getGemLevelMap();
 
         for (ItemStack itemStack : playerInventory.getContents()) {
-            if (!(ItemRegistry.getItemFromItemStack(itemStack) instanceof UpgradeableItem upgradeableItem)) return;
+            if (!(ItemRegistry.getItemFromItemStack(itemStack) instanceof UpgradeableItem upgradeableItem)) continue;
 
             final int storedItemLevel = UpgradeableItem.getItemStackLevel(itemStack) - 1;
             final String rawItemName = upgradeableItem.getRawName();
@@ -69,13 +71,14 @@ public class RerollItem extends Item implements InventoryClickAction {
         playerStats.updateColumn();
 
         final RandomGemStuff.ShowRandomUpgradeableItem showRandomUpgradeableItem = new RandomGemStuff.ShowRandomUpgradeableItem(player);
-        final RandomGemStuff.GiveRandomUpgradeableItem giveRandomUpgradeableItem = new RandomGemStuff.GiveRandomUpgradeableItem(plugin, player, showRandomUpgradeableItem, gemItemStack, RandomGemStuff.getPrimaryColorOfGemName(item.getName()) + "Rerolled Gem");
 
-        Bukkit.getScheduler().runTaskLater(plugin, player::closeInventory, 1L);
-        Bukkit.getScheduler().runTaskLater(plugin, () -> playerInventory.remove(createItem(1)), 2L);
+        final RandomGemStuff.GiveRandomUpgradeableItem giveRandomUpgradeableItem = new RandomGemStuff.GiveRandomUpgradeableItem(plugin, player, showRandomUpgradeableItem, gemItemStack, RandomGemStuff.getPrimaryColorOfGemName(item.getName()) + "Rerolled Gem");
 
         showRandomUpgradeableItem.runTaskTimer(plugin, 1L, 2L);
         giveRandomUpgradeableItem.runTaskLater(plugin, 80L);
+
+        Bukkit.getScheduler().runTaskLater(plugin, player::closeInventory, 1L);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> clickedItemStack.setAmount(0), 2L);
     }
 
     @Override
